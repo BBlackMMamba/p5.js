cb7b3e03445a5f5990419975b126eb3d63da1d90
BBlackMMamba <83463382+BBlackMMamba@users.noreply.github.com>
2025-03-04 13:22:03 +0400

    Final SketchResult

--- sketch.js
+++ sketch.js
@@ -1,23 +1,33 @@
 const canvasSketch = require('canvas-sketch');
+const math = require('canvas-sketch-util/math');
+const random = require('canvas-sketch-util/random');
+const Color = require('canvas-sketch-util/color');
 
 const settings = {
     dimensions: [1080, 1080]
 };
 
-const degToRad = (degrees) => {
+/* const degToRad = (degrees) => {
     return degrees / 180 * Math.PI;
 }
 
 const randomRange = (min, max) => {
     return Math.random() * (max - min) + min;
+} */
+
+function getRandomColor() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r},${g},${b})`;
 }
 
 const sketch = () => {
     return ({ context, width, height }) => {
-        context.fillStyle = 'white';
+        context.fillStyle = getRandomColor();
         context.fillRect(0, 0, width, height);
 
-        context.fillStyle = 'black';
+        context.fillStyle = getRandomColor();
 
         const cx = width * 0.5;
         const cy = height * 0.5;
@@ -25,11 +35,11 @@ const sketch = () => {
         const h = height * 0.1;
         let x, y;
 
-        const num = 12;
+        const num = 40;
 
         for (let i = 0; i < num; i++) {
 
-            const slice = degToRad(360 / num);
+            const slice = math.degToRad(360 / num);
             const angle = slice * i;
             const radius = width * 0.3;
 
@@ -39,11 +49,25 @@ const sketch = () => {
             context.save();
             context.translate(x, y);
             context.rotate(-angle);
-            context.scale(randomRange(1, 3), 1);
+            context.scale(random.range(0.1, 2), random.range(0.5, 0.5));
 
             context.beginPath();
-            context.rect(-w * 0.5, -h * 0.5, w, h);
+            context.rect(-w * 0.5, random.range(0, -h * 0.5), w, h);
             context.fill();
+            context.restore();
+
+            context.save();
+            context.translate(cx, cy);
+            context.rotate(-angle);
+
+            context.lineWidth = random.range(5, 20)
+            context.strokeStyle = getRandomColor()
+
+            context.beginPath();
+            context.arc(0, 0, radius * random.range(0.7, 1.3), slice * random.range(0, -8), slice * random.range(0, 5));
+            context.stroke();
+
+
             context.restore();
 
         }
